5fff70d63667f878f29c14501b1b0452dc4a5def
Yoon joonyeol <raoneli@naver.com>
2023-04-18 23:29:35 +0900

    23-04-18 coding test

--- lv1/test58.py
+++ lv1/test58.py
@@ -1,38 +1,38 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/17682
 # [1차] 다트 게임
 
-문제 설명
- 다트 게임의 점수 계산 로직은 아래와 같다.
+# 문제 설명
+#  다트 게임의 점수 계산 로직은 아래와 같다.
 
-다트 게임은 총 3번의 기회로 구성된다.
-각 기회마다 얻을 수 있는 점수는 0점에서 10점까지이다.
-점수와 함께 Single(S), Double(D), Triple(T) 영역이 존재하고 각 영역 당첨 시 
-점수에서 1제곱, 2제곱, 3제곱 (점수1 , 점수2 , 점수3 )으로 계산된다.
-옵션으로 스타상(*) , 아차상(#)이 존재하며 스타상(*) 당첨 시 해당 점수와 바로 
-전에 얻은 점수를 각 2배로 만든다. 아차상(#) 당첨 시 해당 점수는 마이너스된다.
-스타상(*)은 첫 번째 기회에서도 나올 수 있다. 
-이 경우 첫 번째 스타상(*)의 점수만 2배가 된다. (예제 4번 참고)
-스타상(*)의 효과는 다른 스타상(*)의 효과와 중첩될 수 있다. 
-이 경우 중첩된 스타상(*) 점수는 4배가 된다. (예제 4번 참고)
-스타상(*)의 효과는 아차상(#)의 효과와 중첩될 수 있다. 
- 이 경우 중첩된 아차상(#)의 점수는 -2배가 된다. (예제 5번 참고)
-Single(S), Double(D), Triple(T)은 점수마다 하나씩 존재한다.
-스타상(*), 아차상(#)은 점수마다 둘 중 하나만 존재할 수 있으며, 존재하지 않을 수도 있다.
-0~10의 정수와 문자 S, D, T, *, #로 구성된 문자열이 입력될 시 
-총점수를 반환하는 함수를 작성하라.
+# 다트 게임은 총 3번의 기회로 구성된다.
+# 각 기회마다 얻을 수 있는 점수는 0점에서 10점까지이다.
+# 점수와 함께 Single(S), Double(D), Triple(T) 영역이 존재하고 각 영역 당첨 시 
+# 점수에서 1제곱, 2제곱, 3제곱 (점수1 , 점수2 , 점수3 )으로 계산된다.
+# 옵션으로 스타상(*) , 아차상(#)이 존재하며 스타상(*) 당첨 시 해당 점수와 바로 
+# 전에 얻은 점수를 각 2배로 만든다. 아차상(#) 당첨 시 해당 점수는 마이너스된다.
+# 스타상(*)은 첫 번째 기회에서도 나올 수 있다. 
+# 이 경우 첫 번째 스타상(*)의 점수만 2배가 된다. (예제 4번 참고)
+# 스타상(*)의 효과는 다른 스타상(*)의 효과와 중첩될 수 있다. 
+# 이 경우 중첩된 스타상(*) 점수는 4배가 된다. (예제 4번 참고)
+# 스타상(*)의 효과는 아차상(#)의 효과와 중첩될 수 있다. 
+#  이 경우 중첩된 아차상(#)의 점수는 -2배가 된다. (예제 5번 참고)
+# Single(S), Double(D), Triple(T)은 점수마다 하나씩 존재한다.
+# 스타상(*), 아차상(#)은 점수마다 둘 중 하나만 존재할 수 있으며, 존재하지 않을 수도 있다.
+# 0~10의 정수와 문자 S, D, T, *, #로 구성된 문자열이 입력될 시 
+# 총점수를 반환하는 함수를 작성하라.
 
-입력 형식
-"점수|보너스|[옵션]"으로 이루어진 문자열 3세트.
-예) 1S2D*3T
+# 입력 형식
+# "점수|보너스|[옵션]"으로 이루어진 문자열 3세트.
+# 예) 1S2D*3T
 
-점수는 0에서 10 사이의 정수이다.
-보너스는 S, D, T 중 하나이다.
-옵선은 *이나 # 중 하나이며, 없을 수도 있다.
-출력 형식
-3번의 기회에서 얻은 점수 합계에 해당하는 정수값을 출력한다.
-예) 37
+# 점수는 0에서 10 사이의 정수이다.
+# 보너스는 S, D, T 중 하나이다.
+# 옵선은 *이나 # 중 하나이며, 없을 수도 있다.
+# 출력 형식
+# 3번의 기회에서 얻은 점수 합계에 해당하는 정수값을 출력한다.
+# 예) 37
 
-dartResult = '1S2D*3T'
+
 # score = []
 # for i in dartResult:
 #     num = 0
@@ -47,12 +47,76 @@ dartResult = '1S2D*3T'
 #     score.append(num)
 # print(score)
 
+
+# replace랑 eval 써서 풀어보기
+
+# for idx,value in enumerate(dartResult):
+#     if value == '#' or value == '*':
+        # option.append(idx)
+
 # 첫숫자**첫문자*(가중치)+ 이런형태임
-def score(num, string, add=None):
+def score(num, string):
     if string=='S': string = 1
     elif string=='D': string = 2
     elif string =='T': string = 3
-    if add == None: add = 1
-    elif add == '#': add = -1
+    answer = int(num)**string
+    return answer
 
-    answer = num**string*(add)
+# 앞에서 3개를 검색하고 옵션 확인
+# 10일때 처리 해야함
+# while len(dartResult) >= 2:
+#     if len(dartResult) == 2:
+#         arr.append(dartResult[:2])
+#         arr.append('')
+#         break
+#     if dartResult[2] == '*' or dartResult[2] == '#':
+#         arr.append(dartResult[:2])
+#         arr.append(dartResult[2])
+#         dartResult = dartResult[3:]
+#     else :
+#         arr.append(dartResult[:2])
+#         arr.append('')
+#         dartResult = dartResult[2:]
+dartResult = '1S*2T*3S'
+arr = []
+while len(dartResult) != 0:
+    if dartResult[0:2] == '10':
+        arr.append(dartResult[0:2])
+        dartResult = dartResult[2:]
+    else: 
+        arr.append(dartResult[0:1])
+        dartResult = dartResult[1:]
+    if dartResult[0] == 'S':
+        arr.append('**1')
+    elif dartResult[0] == 'D':
+        arr.append('**2')
+    elif dartResult[0] == 'T':
+        arr.append('**3')
+    dartResult = dartResult[1:]
+    if len(dartResult)==0: break
+    if dartResult[0] == '*' or dartResult[0] == '#':
+        arr.append(dartResult[0])
+        dartResult = dartResult[1:]
+    else:
+        arr.append('+')
+print(arr)
+for idx,value in enumerate(arr[::]):
+    if value == '*':
+        arr[idx] = '*2+'
+        if idx != 2:
+            arr[idx-3] = '*2'+arr[idx-3]
+    elif value == '#':
+        arr[idx] = '*(-1)+'
+sum_str = ''.join(arr)
+print(sum_str)
+if sum_str[-1] =='+': sum_str=sum_str[:-1]
+answer = eval(sum_str)
+
+print(answer)
+# answer = arr[0]+arr[3]+arr[6]
+# print(arr)
+# print(answer)
+
+
+
+
